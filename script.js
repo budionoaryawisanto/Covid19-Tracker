6d4217ea4594f30188997c8987708a5698497eed
budionoaryawisanto <budionoarya.w@gmail.com>
2023-10-26 11:22:52 +0700

    assignment 3 donee

--- script.js
+++ script.js
@@ -1,43 +1,68 @@
-console.log("ss")
 let btn=document.getElementById('btn');
 let span=document.getElementById("span");
-span.style.display="none"
+span.style.display = "none"
 btn.addEventListener('click',()=>{
-    span.style.display="block"
+    span.style.display = "block"
     let input=document.getElementById('text');
-    let Container=document.getElementsByClassName('container')[0];
-
-getdata();
-    function getdata(){
-        url='https://api.covid19api.com/summary';
-        fetch(url).then((response)=>{
-            return response.json()
-        }).then((data)=>{
-            let country=data.Countries;
-            let html="";
-            for(let i=0;i<country.length;i++){
-                if(country[i].Slug==input.value.toLowerCase() ||country[i].Country.toUpperCase()==input.value.toUpperCase()){
-                    console.log(country[i].Slug)
-html=`<h3>${country[i].Country}</h3>
-<div id="card-wrapper">
-    <div class="card">
-        <span>${country[i].NewConfirmed}</span>NEW CASES
-    </div>
-    <div class="card">
-        <span>${country[i].TotalConfirmed}</span>TOTAL CASES
-    </div>
-    <div class="card">
-        <span>${country[i].NewDeaths}</span>NEW DEATH
-    </div>
-    <div class="card">
-        <span>${country[i].TotalDeaths}</span>TOTAL DEATH
-    </div>
-</div>`
-span.style.display="none"
-Container.innerHTML=html;
-                }
+    let Container = document.getElementsByClassName('container')[0];
+    Container.innerHTML = ""
+    function convert(angka){
+	var rupiah = '';		
+	var angkarev = angka.toString().split('').reverse().join('');
+	for(var i = 0; i < angkarev.length; i++) if(i%3 == 0) rupiah += angkarev.substr(i,3)+'.';
+	return rupiah.split('',rupiah.length-1).reverse().join('');
+    }
+    getdata();
+    async function getdata(){
+        const url = 'https://covid-193.p.rapidapi.com/statistics';
+        const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'eadc9bd239mshe53f4719d2f610fp14d7bfjsn4b911acdc4d6',
+		'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
             }
-            console.log(country[0].Country)
+        };
+        await fetch(url, options).then((response)=>{
+            return response.json()
+        }).then((data) => {
+            let datas = data.response;
+            let html = ""
+            for (let i = 0; i < datas.length; i++){
+                if (input.value.toLowerCase() == datas[i].country.toLowerCase()){
+                    let html =`<h3>${datas[i].country}</h3>
+                    <div id="card-wrapper">
+                        <div class="card">
+                            <span>${convert(datas[i].cases.active)}</span>
+                            <div>ACTIVE CASES</div>
+                        </div>
+                        <div class="card">
+                            <span>${datas[i].cases.new}</span>
+                            <div>NEW CASES</div>
+                        </div>
+                        <div class="card">
+                            <span>${convert(datas[i].cases.recovered)}</span>
+                            <div>RECOVERED CASE</div>
+                        </div>
+                        <div class="card">
+                            <span>${convert(datas[i].cases.total)}</span>
+                            <div>TOTAL CASE</div>
+                        </div>
+                        <div class="card">
+                            <span>${convert(datas[i].deaths.total)}</span>
+                            <div>TOTAL DEATH</div>
+                        </div>
+                        <div class="card">
+                            <span>${convert(datas[i].tests.total)}</span>
+                            <div>TOTAL TEST</div>
+                        </div>
+                    </div>`
+                    span.style.display = "none"
+                    Container.innerHTML = html;
+                }
+            } if (Container.innerHTML == "") {
+                span.style.display = "none"
+                alert("Not Found Your Data !")
+            }
         })
     }
 })
